7f5bf9b0283dfa68418d8b378f7a1736fc26cd73
ThisaraJayas <sasmithajayasinghe1@gmail.com>
2025-04-22 22:57:32 +0530

    additional

--- src/main/java/com/auth/auth_service/model/User.java
+++ src/main/java/com/auth/auth_service/model/User.java
@@ -11,9 +11,27 @@ public class User {
 
     @Id
     private String id;
+    private String fname;
+    private String lname;
     private String email;
     private String password;
     private UserRole userType;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
 
     public UserRole getUserType() {
         return userType;
